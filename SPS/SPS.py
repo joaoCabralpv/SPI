e5d68b3614bf42e2a9416c9141b19410eacbed95
joaoCabralpv <joaocabralpv@gmail.com>
2022-07-23 09:42:44 +0000

    comentarios e inicio da calculadora

--- SPS/SPS.py
+++ SPS/SPS.py
@@ -1,26 +1,54 @@
-listaprogramas = ["calculadora", "cat", "texto aleatório", "sair"]
+listaProgramas = ["calculadora", "cat", "texto aleatório", "sair"]
 primeira_vez = True
 
 def calculadora() :
-    print("calculadora")
+    listaOperacoes = ["soma","subtetração", "mutiplicação", "divisão", "porcentagem", "módulo", "expoente", "raiz quadrada"]
+    igual = "="*19
+    print(igual)
+    print("escolha a operação")
+    print(igual)
+
+
+    for i in range(len(listaOperacoes)):
+        iponto = str(i+1) + ". "
+        print(iponto, listaOperacoes[i])
+    op = 0;    
+    while True:
+        op = input("Qual é a operação? ")
+        if op.isnumeric() :
+            if int(op) >= len(listaOperacoes)+1 and int(op) >= 1:
+                break
+        print("\033[;31;m erro: operação inválida\033[;;;m")
+    
+    n1 = int(input("Qual é o 1º número? "))
+    if listaOperacoes[op] is not "raiz quadrada":
+        n2 = int(input("Qual é o 2º número? "))
+
+    
+
+
 def cat() :
     print("cat")
 def texto_aleatorio() :
     print("texto aleatorio")
 
 def inicio():
+    # iniacialização
     primeira_vez = False
     igual = "=" * 19
     print(igual)
     print("escolha a aplicação")
     print(igual,"\n")
 
-    for i in range(len(listaprogramas)):
+    # escreve os programas para o console
+    for i in range(len(listaProgramas)):
         iponto = str(i+1) + ". "
-        print(iponto,  listaprogramas[i])
+        print(iponto,  listaProgramas[i])
+
+    # escolhe o programa na suite
 
     escolha = input("Qual é a aplicação que queres usar: ")
-    if escolha.isnumeric() and int(escolha) <= len(listaprogramas)+1:
+    if escolha.isnumeric() and int(escolha) <= len(listaProgramas)+1:
         if escolha == "1" :
             calculadora()
         elif escolha == "2" :
