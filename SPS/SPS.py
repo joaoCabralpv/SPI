f9d6519227bf8d30e20b40a48022a22acc525b44
joaoCabralpv <joaocabralpv@gmail.com>
2022-08-13 13:57:22 +0000

    Sair melhorado

--- SPS/SPS.py
+++ SPS/SPS.py
@@ -1,9 +1,17 @@
 from math import factorial
+from operator import truediv
 from random import randint
 
 
 listaProgramas = ["calculadora", "cat", "texto aleatório", "sair"]
 primeira_vez = True
+
+def sair() :
+    return
+
+def guardarArquivo(texto) :
+    arquivo = open(input("Qual é o nome do arquivo? Se tiver um arquivo com o mesmo nome, o arquivo será apagado: "), "w")
+    arquivo.write(str(texto))
 
 def calculadora() :
     listaOperacoes = ["soma","subtetração", "mutiplicação", "divisão", "porcentagem", "módulo", "expoente", "raiz quadrada", "raiz cúbica", "média", "maior", "menor", "factorial"]
@@ -39,37 +47,63 @@ def calculadora() :
     if listaOperacoes[iop] != "raiz quadrada" and listaOperacoes[iop] != "raiz cúbica" and listaOperacoes[iop] != "factorial":
         n2 = int(input("Qual é o segundo número: "))
 
+    guardar = input("Queres grardar o resultado para um arquivo? [Y/N] : ").strip().upper()
+    print()
+
     # executa a operação e escreve o resultado para o console
 
+    texto = 0
+    resultado = 0
+
     if iop == 0:
-        print("A soma entre {} e {} é {} ".format(n1,n2,n1+n2))
+        resultado = n1+n2
+        texto ="A soma entre {} e {} é {} ".format(n1,n2,resultado)
     elif iop == 1:
-        print("A difrença entre {} e {} é {}".format(n1,n2,n1-n2))
+        resultado = n1-n2
+        texto = "A difrença entre {} e {} é {}".format(n1,n2,resultado)
     elif iop == 2:
-        print("O produto entre {} e {} é {}".format(n1,n2,n1*n2))
+        resultado = n1*n2
+        texto = "O produto entre {} e {} é {}".format(n1,n2, resultado)
     elif iop == 3:
-        print("O quociente entre {} e {} é {}".format(n1,n2,n1/n2))
+        resultado = n1/n2
+        texto = "O quociente entre {} e {} é {}".format(n1,n2,resultado)
     elif iop == 4:
-        print("{} módulo de {} é {}".format(n1,n2,n1%n2))
+        resultado = n1*(100/n2)
+        texto = "{} porcento de {} é {}".format(n1,n2,resultado)
     elif iop == 5:
-        print("{} porcento de {} é {}".format(n1,n2,n1*(100/n2)))
+         resultado = n1%n2
+         texto ="{} módulo de {} é {}".format(n1,n2,resultado)
     elif iop == 6:
-        print("{} elevado a {} é {}".format(n1,n2,n1**n2))
+        resultado = n1**n2
+        texto = "{} elevado a {} é {}".format(n1,n2,resultado)
     elif iop == 7:
-        print("A raiz quadrada de {} é {}".format(n1,n1**0.5))
+        resultado = n1**0.5
+        texto = "A raiz quadrada de {} é {}".format(n1,resultado)
     elif iop == 8:
-        print("A raiz cúbica de {} é {}".format(n1,n1**(1/3)))
+        resultado = n1**(1/3)
+        texto = "A raiz cúbica de {} é {}".format(n1,)
     elif iop == 9:
-        print("A média entre {} e {} é {}".format(n1,n2,((n1+n2)/2)))
+        resultado = (n1+n2)/2
+        texto = "A média entre {} e {} é {}".format(n1,n2,resultado)
     elif iop == 10:
-        print("Entre {} e {}, o maior é {}".format(n1,n2,(max(n1,n2))))
+        resultado = (max(n1,n2))
+        texto = "Entre {} e {}, o maior é {}".format(n1,n2,resultado)
     elif iop == 11:
-        print("Entre {} e {}, o menor é ".format(n1,n2,min(n1,n2)))
+        resultado = min(n1,n2)
+        texto = "Entre {} e {}, o menor é ".format(n1,n2,)
     elif iop == 12:
-        print("{} factorial é {}".format(n1,factorial(n1)))
+        resultado = factorial(n1)
+        texto = "{} factorial é {}".format(n1,resultado)
     else:
         print("Isso não devia acontecer")
 
+    # escreve o resultado para o terminal e guarda o resultado para um arquivo se o usário pediu  
+
+    print(texto)
+    print("\n")
+    if guardar == "Y":
+        guardarArquivo(resultado)
+   
     # volta para o início do programa
 
     return inicio()
@@ -92,22 +126,21 @@ def texto_aleatorio() :
     print()
     return inicio()
 
-def inicio():
-        # iniacialização
-        print("\n")
-        igual = "=" * 19
-        print(igual)
-        print("escolha a aplicação")
-        print(igual,"")
+def inicio(): 
+            # iniacialização
+            print("\n")
+            igual = "=" * 19
+            print(igual)
+            print("escolha a aplicação")
+            print(igual,"")
 
-        # escreve os programas para o console
-        for i in range(len(listaProgramas)):
-            iponto = str(i+1) + ". "
-            print(iponto,  listaProgramas[i])
+            # escreve os programas para o console
+            for i in range(len(listaProgramas)):
+                iponto = str(i+1) + ". "
+                print(iponto,  listaProgramas[i])
 
-        # escolhe o programa na suite
+            # escolhe o programa na suite
 
-        while True:
             escolha = input("Qual é a aplicação que queres usar: ")
             if escolha.isnumeric() and int(escolha) <= len(listaProgramas)+1:
                 if escolha == "1" :
@@ -116,11 +149,13 @@ def inicio():
                     cat()
                 elif escolha == "3" :
                     texto_aleatorio()
-                elif escolha == "4":
-                    return
+                elif escolha != "4" :
+                    inicio()
             else :
                 print("\033[;31;m erro: operação inválida\033[;;;m")
+                inicio()
 
 if primeira_vez:
     primeira_vez = False
-    inicio()
+    a = inicio()
+
